59e6d7ddaf7d688a85ccb0e24ed6c89a647902ab
IceHole <icezgan21@gmail.com>
2023-03-27 20:47:19 +0300

    test commit

---
+++ source/Source.cpp
@@ -0,0 +1,7 @@
+#include "list.h"
+#include "polynomial.h"
+
+int main() {
+	//polynomial abc = { {1, 0, 0, 0}, {1, 1, 0, 0}, {2, 1, 0, 0}, {3, 1, 0, 0}, {12, 0, 1, 0}, {14, 0, 0, 1}, {23, 1, 1, 0}, {43, 1, 1, 1} };
+	return 0;
+}
